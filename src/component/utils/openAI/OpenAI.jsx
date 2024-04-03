50f19bd3ec670c4b69a07fdabb8e7d9de12fbdb9
Prashant Badal <p26badal@gmail.com>
2024-04-03 13:30:41 +0530

    ok

--- src/component/utils/openAI/OpenAI.jsx
+++ src/component/utils/openAI/OpenAI.jsx
@@ -1,5 +1,5 @@
 import OpenAI from 'openai';
-import { OPENAI_KEY } from '../constant/constant';
+import { OPENAI_KEY} from '../constant/constant';
 
 const openai = new OpenAI({
   apiKey:OPENAI_KEY, 
