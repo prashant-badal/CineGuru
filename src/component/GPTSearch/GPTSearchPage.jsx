ee47b91d7b5adc037c82dadc395016d21c590a0e
Prashant Badal <p26badal@gmail.com>
2024-04-03 14:33:28 +0530

    till suggestion

--- src/component/GPTSearch/GPTSearchPage.jsx
+++ src/component/GPTSearch/GPTSearchPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import GptSearchBar from './GptSearchBar'
 import { BG_URL } from '../utils/constant/constant'
+import GptSuggestionMovies from './GptSuggestionMovies'
 
 const GPTSearchPage = () => {
   return (
     <div>
-      <div  className='absolute z-[-12]'>
+      <div  className='fixed z-[-12]'>
         <img src={BG_URL} alt="bgImg" />
       </div>
       <GptSearchBar/>
+      <GptSuggestionMovies/>
     </div>
   )
 }
